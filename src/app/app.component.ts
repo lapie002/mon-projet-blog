39774ef0ca00af328e3db4f6258778f6f4c14287
Bruno <lapierre.bruno@gmail.com>
2018-03-20 09:40:54 -0400

    changement de posts en post-list et 2 component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app';
 
-
-  posts = [
-
+  postsTab = [
     {
       title:'Mon premier post',
       content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
